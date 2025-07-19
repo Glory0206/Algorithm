5af85a4f3e19db42d79516a2b4a6edeb2781f3bf
Yeonggwan Seo <113930407+Glory0206@users.noreply.github.com>
2025-07-19 16:17:52 +0900

    [Silver III] Title: 계단 오르기, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.py
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.py
@@ -10,6 +10,8 @@ dp[1] = stair[1]
 if n >= 2:
     dp[2] = stair[1] + stair[2]
 
+# dp[i] =  stairs[i] + dp[i-2]: 마지막 계단 + 마지막에서 2칸 떨어져있는 계단까지의 값
+# dp[i] = stair[i] + stair[i-1] + dp[i - 3]: 마지막 계단 + 마지막 1칸 전 계단 + 1칸전의 계단에서 2칸 떨어져있는 계단까지의 값
 for i in range(3, n+1):
     dp[i] = max(dp[i - 2], dp[i - 3] + stair[i - 1]) + stair[i]
 
