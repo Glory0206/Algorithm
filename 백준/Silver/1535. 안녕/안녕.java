449ffaa877edd90c2280b3131e535f3e22d05175
Yeonggwan Seo <113930407+Glory0206@users.noreply.github.com>
2025-12-09 20:28:55 +0900

    [Silver II] Title: 안녕, Time: 108 ms, Memory: 14256 KB -BaekjoonHub

---
+++ 백준/Silver/1535. 안녕/안녕.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+  public static void main(String args[]) throws IOException{
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    int N = Integer.parseInt(br.readLine()); // 사람의 수
+
+    int[] dp = new int[101];
+
+    int[] health = new int[N+1];
+    int[] happy = new int[N+1];
+    int heart = 100;
+
+    StringTokenizer st = new StringTokenizer(br.readLine());
+    for(int i=1; i<=N; i++){
+      health[i] = Integer.parseInt(st.nextToken());
+    }
+    st = new StringTokenizer(br.readLine());
+    for(int i=1; i<=N; i++){
+      happy[i] = Integer.parseInt(st.nextToken());
+    }
+
+    for(int i=1; i<=N; i++){
+      for(int j=100; j>health[i]; j--){ // health가 더 낮으면 말할 수 없기 때문에
+        dp[j] = Math.max(dp[j], dp[j-health[i]]+happy[i]); // 사람과 말하기, 말하지 않고 넘어가기
+      } 
+    }
+    System.out.println(dp[100]);
+  }
+}
